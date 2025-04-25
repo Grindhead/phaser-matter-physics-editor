681eb80d8ebc3fd88e9fd6cb2264b85c40913eac
craig beswetherick <craigbeswetherick@gmail.com>
2025-04-25 01:18:32 +0100

    new build

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,6 @@ const config: Types.Core.GameConfig = {
         x: 0,
         y: 1,
       },
-      debug: true,
     },
   },
   scene: [Boot, Preloader, MainMenu, MainGame, DebugUIScene, GameOver],

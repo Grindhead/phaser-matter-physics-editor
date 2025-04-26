d56bdf9131c08fb5c27f420c3cdde2a2d0b4b3cb
craig beswetherick <craigbeswetherick@gmail.com>
2025-04-26 09:25:36 +0100

    fixed zooming

--- src/lib/ui/CameraManager.ts
+++ src/lib/ui/CameraManager.ts
@@ -5,8 +5,8 @@ const WORLD_WIDTH = 10000;
 const WORLD_HEIGHT = 4000;
 // Define zoom levels
 const BASE_ZOOM = 1.5; // Reduced base zoom
-const DEATH_ZOOM_IN = 1.5; // Zoom level on death
-const DEATH_ZOOM_DURATION = 700; // Duration of death zoom tween in ms
+const ZOOM_IN = 2; // Zoom level on death - Increased for noticeable effect
+const ZOOM_DURATION = 700; // Duration of death zoom tween in ms
 
 export class CameraManager {
   private scene: Scene;
@@ -44,9 +44,9 @@ export class CameraManager {
   handleZoomIn(): void {
     this.scene.tweens.add({
       targets: this.camera,
-      zoom: DEATH_ZOOM_IN,
-      duration: DEATH_ZOOM_DURATION,
-      ease: "Power2", // Use an easing function for smoothness
+      zoom: ZOOM_IN,
+      duration: ZOOM_DURATION,
+      ease: "Power2",
     });
   }
 }
